cf0e690ed1acb3a42728f31a1231938ccb96684a
boyesky <ogunrindeboye@gmail.com>
2025-09-29 01:01:40 -0400

    update

--- canvas.js
+++ canvas.js
@@ -18,7 +18,7 @@ ctx.moveTo(340, 70);
 ctx.lineTo(270, 20);
 ctx.stroke();
 
-ctx.fillStyle = "grey";
+ctx.fillStyle = " #bfb791ff";
 ctx.fillRect(20, 70, 322, 120);
 
 ctx.fillStyle = "white";
@@ -38,17 +38,17 @@ ctx.fillStyle = "brown";
 ctx.arc(193, 150, 3, 0, 2 * Math.PI);
 ctx.fill();
 
-let x = 0;
-let speed = 2;
+let x = 30;
+let speed = 4;
 
 function animate() {
   ctx.beginPath();
   ctx.clearRect(345, 0, 110, canvas.height);
-  ctx.fillStyle = "grey";
+  ctx.fillStyle = " #a59d9d";
   ctx.arc(420, x, 30, 0, 2 * Math.PI);
   ctx.fill();
   x += speed;
-  if (x + 30 >= canvas.height || x <= 0) {
+  if (x + 30 >= canvas.height || x - 30 < 0) {
     speed = -speed;
   }
 
